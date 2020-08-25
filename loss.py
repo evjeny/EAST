3afad21c99b9da5fb310bc5e70f79aaceaf7dbd5
evjeny <gkanafing@gmail.com>
2020-08-25 10:06:13 +0000

    add logging to file and console

--- loss.py
+++ loss.py
@@ -23,9 +23,10 @@ def get_geo_loss(gt_geo, pred_geo):
 
 
 class Loss(nn.Module):
-	def __init__(self, weight_angle=10):
+	def __init__(self, logger, weight_angle=10):
 		super(Loss, self).__init__()
 		self.weight_angle = weight_angle
+		self.logger = logger
 
 	def forward(self, gt_score, pred_score, gt_geo, pred_geo, ignored_map):
 		if torch.sum(gt_score) < 1:
@@ -37,5 +38,5 @@ class Loss(nn.Module):
 		angle_loss = torch.sum(angle_loss_map*gt_score) / torch.sum(gt_score)
 		iou_loss = torch.sum(iou_loss_map*gt_score) / torch.sum(gt_score)
 		geo_loss = self.weight_angle * angle_loss + iou_loss
-		print('classify loss is {:.8f}, angle loss is {:.8f}, iou loss is {:.8f}'.format(classify_loss, angle_loss, iou_loss))
+		self.logger.info('classify loss is {:.8f}, angle loss is {:.8f}, iou loss is {:.8f}'.format(classify_loss, angle_loss, iou_loss))
 		return geo_loss + classify_loss
--- train.py
+++ train.py
@@ -7,16 +7,19 @@ from model import EAST
 from loss import Loss
 import os
 import time
+import logging
 import numpy as np
 
 
-def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, interval, start_from=None, start_from_epoch=0):
+def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, interval, logger, start_from=None, start_from_epoch=0):
+	logger.info("begin training")
+	
 	file_num = len(os.listdir(train_img_path))
 	trainset = custom_dataset(train_img_path, train_gt_path)
 	train_loader = data.DataLoader(trainset, batch_size=batch_size, \
                                    shuffle=True, num_workers=num_workers, drop_last=True)
 	
-	criterion = Loss()
+	criterion = Loss(logger)
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 	model = EAST()
 	data_parallel = False
@@ -26,8 +29,11 @@ def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers,
 	model.to(device)
 	if start_from:
 		model.load_state_dict(torch.load(start_from))
+		logger.info("start from weights {}".format(start_from))
 	if start_from_epoch:
-		model.load_state_dict(torch.load(os.path.join(pths_path, "model_epoch_{}.pth".format(start_from_epoch))))
+		weights_path = "model_epoch_{}.pth".format(start_from_epoch)
+		model.load_state_dict(torch.load(os.path.join(pths_path, weights_path)))
+		logger.info("start from epoch {}".format(weights_path))
 	optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 	scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[epoch_iter//2], gamma=0.1)
 
@@ -46,12 +52,12 @@ def train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers,
 			loss.backward()
 			optimizer.step()
 
-			print('Epoch is [{}/{}], mini-batch is [{}/{}], time consumption is {:.8f}, batch_loss is {:.8f}'.format(\
+			logger.info('Epoch is [{}/{}], mini-batch is [{}/{}], time consumption is {:.8f}, batch_loss is {:.8f}'.format(\
               epoch+1, epoch_iter, i+1, int(file_num/batch_size), time.time()-start_time, loss.item()))
 		
-		print('epoch_loss is {:.8f}, epoch_time is {:.8f}'.format(epoch_loss/int(file_num/batch_size), time.time()-epoch_time))
-		print(time.asctime(time.localtime(time.time())))
-		print('='*50)
+		logger.info('epoch_loss is {:.8f}, epoch_time is {:.8f}'.format(epoch_loss/int(file_num/batch_size), time.time()-epoch_time))
+		logger.info(str(time.asctime(time.localtime(time.time()))))
+		logger.info('='*50)
 		if (epoch + 1) % interval == 0:
 			state_dict = model.module.state_dict() if data_parallel else model.state_dict()
 			torch.save(state_dict, os.path.join(pths_path, 'model_epoch_{}.pth'.format(epoch+1)))
@@ -62,10 +68,22 @@ if __name__ == '__main__':
 	train_gt_path  = "/big_disk/evjeny/data/generated_perimetry_labels_big"
 	pths_path      = './pths'
 	start_from = "./pths/model_small_dataset_last.pth"
-	start_from_epoch = 0
+	start_from_epoch = 1
 	batch_size     = 6
 	lr             = 1e-3
 	num_workers    = 4
 	epoch_iter     = 600
 	save_interval  = 1
-	train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, save_interval, start_from, start_from_epoch)
+	
+	logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+	rootLogger = logging.getLogger()
+	rootLogger.setLevel(logging.INFO)
+
+	fileHandler = logging.FileHandler("log_train.txt")
+	fileHandler.setFormatter(logFormatter)
+	rootLogger.addHandler(fileHandler)
+
+	consoleHandler = logging.StreamHandler()
+	consoleHandler.setFormatter(logFormatter)
+	rootLogger.addHandler(consoleHandler)
+	train(train_img_path, train_gt_path, pths_path, batch_size, lr, num_workers, epoch_iter, save_interval, rootLogger, start_from, start_from_epoch)
